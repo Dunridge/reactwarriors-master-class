c34e4ccebef899905f0d7c784e630fe51e5ad8df
Maksym Polinka <Polinka7max@gmail.com>
2020-03-23 09:45:58 +0200

    added pagination features

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'; //adding lifecycle hooks
 import './App.css';
 // import { movieData } from './components/movieData';
 import MovieItem from './components/movieItem';
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { movieData } from './components/movieData';
 import MovieTabs from './components/MovieTabs';
 //let title = 'Hello, World!';
+import Pagination from './components/Pagination';
 
 //UI = function(state, props)
 class App extends React.Component {
@@ -14,7 +15,8 @@ class App extends React.Component {
         this.state = {
             movies: [], //movieData // []
             moviesWillWatch: [],
-            sort_by: 'revenue.desc'
+            sort_by: 'revenue.desc',
+            current_page: 1
         };
         console.log('constructor');
     }
@@ -26,10 +28,10 @@ class App extends React.Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
-        console.log("did update");
+        console.log('did update');
         console.log('prev', prevProps, prevState);
         console.log('this', this.props, this.state);
-        if(prevState.sort_by !== this.state.sort_by) {
+        if (prevState.sort_by !== this.state.sort_by) {
             console.log('call api');
             this.getMovies();
         }
@@ -48,7 +50,7 @@ class App extends React.Component {
                 console.log('data: ', data);
                 this.setState({ movies: data.results });
             });
-    }
+    };
 
     //callback
     handleDelete = movie => {
@@ -95,6 +97,16 @@ class App extends React.Component {
         });
     };
 
+    setCurrentPage = pageNumber => {
+        this.setState({
+            current_page: pageNumber
+        });
+        console.log(this.state);
+    };
+
+    // Change page
+    paginate = pageNumber => this.setCurrentPage(pageNumber);
+
     render() {
         // console.log(this);
         console.log('render', this.state.sort_by);
@@ -108,26 +120,60 @@ class App extends React.Component {
                                     sort_by={this.state.sort_by}
                                     updateSortBy={this.updateSortBy}
                                 />
+
+                                <Pagination
+                                    postsPerPage={6}
+                                    totalPosts={this.state.movies.length}
+                                    paginate={this.paginate}
+                                />
                             </div>
                         </div>
                         <div className="row">
-                            {this.state.movies.map(movie => {
-                                return (
-                                    <div className="col-6 mb-4" key={movie.id}>
-                                        <MovieItem
-                                            movie={movie}
-                                            // movies={this.state.movies}
-                                            removeMovie={this.removeMovie}
-                                            addMovieToWillWatch={
-                                                this.addMovieToWillWatch
-                                            }
-                                            removeMovieFromWillWatch={
-                                                this.removeMovieFromWillWatch
-                                            }
-                                        />
-                                    </div>
-                                );
-                            })}
+                            {//TODO: set a filtering method for the movies array that will return
+                            //another array that you'll map
+                            this.state.movies
+                                .filter(movie => {
+                                    if (this.state.current_page === 1) {
+                                        if (
+                                            this.state.movies
+                                                .slice(0, 5)
+                                                .includes(movie)
+                                        ) {
+                                            return true;
+                                        }
+                                    } else if (this.state.current_page === 2) {
+                                        if(this.state.movies.slice(6, 11).includes(movie)) {
+                                            return true;
+                                        }
+                                    } else if (this.state.current_page === 3) {
+                                        if(this.state.movies.slice(12, this.state.movies.length).includes(movie)) {
+                                            return true;
+                                        }
+                                    }
+                                })
+                                .map(movie => {
+                                    //TODO: ouput the number of pages depending on the number of current page
+
+                                    return (
+                                        <div
+                                            className="col-6 mb-4"
+                                            key={movie.id}
+                                        >
+                                            <MovieItem
+                                                movie={movie}
+                                                // movies={this.state.movies}
+                                                removeMovie={this.removeMovie}
+                                                addMovieToWillWatch={
+                                                    this.addMovieToWillWatch
+                                                }
+                                                removeMovieFromWillWatch={
+                                                    this
+                                                        .removeMovieFromWillWatch
+                                                }
+                                            />
+                                        </div>
+                                    );
+                                })}
                         </div>
                     </div>
                     <div className="col-3">
