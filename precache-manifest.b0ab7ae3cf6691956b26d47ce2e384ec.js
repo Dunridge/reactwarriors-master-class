2e46dbfb777b6c2ebc04ba382319fcc39a284122
Maksym Polinka <Polinka7max@gmail.com>
2020-03-24 08:40:45 +0200

    Updates

---
+++ precache-manifest.b0ab7ae3cf6691956b26d47ce2e384ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ed07de1a027eeee2276a123b9b84ee",
+    "url": "/reactwarriors-master-class/index.html"
+  },
+  {
+    "revision": "c582b7813590303f22b6",
+    "url": "/reactwarriors-master-class/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "cdef30d59875832d9351",
+    "url": "/reactwarriors-master-class/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c582b7813590303f22b6",
+    "url": "/reactwarriors-master-class/static/js/2.4634a762.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactwarriors-master-class/static/js/2.4634a762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdef30d59875832d9351",
+    "url": "/reactwarriors-master-class/static/js/main.485c5ffc.chunk.js"
+  },
+  {
+    "revision": "ff59aa304dba419c6d50",
+    "url": "/reactwarriors-master-class/static/js/runtime-main.b1b05135.js"
+  }
+]);
